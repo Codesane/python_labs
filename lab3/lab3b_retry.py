755600c9a8341c471c14da646b64bd7308bcadca
= <=>
2013-09-10 23:51:34 +0200

    misinterpret lab3B fixed it.

---
+++ lab3/lab3b_retry.py
@@ -0,0 +1,25 @@
+
+LOGIC = {"ELLER" : 1, "OCH" : 2}
+TEXT_TO_BOOL = {"falskt" : False, "sant" : True}
+
+def logikvarde(st, vars, bVal = True):
+	vars["sant"] = "sant"
+	vars["falskt"] = "falskt"
+	if st[0] == "ICKE":
+		if isinstance(st[1], str):
+			if st[1] in vars.keys():
+				return not TEXT_TO_BOOL[vars[st[1]]] if bVal else TEXT_TO_BOOL[vars[st[1]]]
+		return logikvarde(st[1], vars, not bVal)
+	elif st[0] in vars.keys():
+		lhBool = logikvarde(st[2], vars)
+		vlVar = TEXT_TO_BOOL[vars[st[0]]]
+		return (lhBool + vlVar) >= LOGIC[st[1]] if bVal else not (lhBool + vlVar) >= LOGIC[st[1]]
+	elif isinstance(st, str):
+		return TEXT_TO_BOOL[vars[st]]
+	else:
+		return bVal;
+
+
+
+print(logikvarde(["ICKE", ["ICKE", ["ICKE", ["katten_sover", "ELLER", ["ICKE", "katten_sover"]]]]],
+               {"katten_sover": "falskt"}))
