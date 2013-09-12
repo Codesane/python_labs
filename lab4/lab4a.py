b0fdd70320fc9b5f09a65fb18e94b43414a883a7
= <=>
2013-09-12 19:48:00 +0200

    lab4a

--- lab4/lab4a.py
+++ lab4/lab4a.py
@@ -1,17 +1,24 @@
-
+# lab4A.py
+# @author Felix Ekdahl
+# Student-ID: felno295
+# Assignment 4A from: http://www.ida.liu.se/~TDDD64/python/la/la4.shtml
 
 
 def powerset(lst):
 	if not lst:
 		return [[]]
 	applist = []
-	for i in powerset(lst[1:]):
-		subs = []
-		subs.append(lst[0])
-		subs.extend(i)
-		applist.append(subs)
-		applist.append(i)
-	return  applist
+	applist.extend(powerset2(lst, powerset(lst[1:]), []))
+	return applist
 
 
-print(powerset(['a', 'b', 'c']))
+def powerset2(lst, iter, retList = []):
+	if not iter:
+		return retList
+	subs = list(lst[0]) + list(iter[0])
+	retList.append(subs)
+	retList.append(iter[0])
+	return powerset2(lst, iter[1:], retList)
+
+
+print(powerset(['a', 'b']))
