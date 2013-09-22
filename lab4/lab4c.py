d3ba12eef62f4894cce24f134e0f32afa7dc6ad7
= <=>
2013-09-23 01:12:02 +0200

    lab4c.py

--- lab4/lab4c.py
+++ lab4/lab4c.py
@@ -2,25 +2,15 @@ import math
 
 dx = 0.001
 
-def glatta(func):
-	return lambda x: ((func(x - dx) + func(x) + func(x + dx)) / 3)
-
-
+def glatta(fn):
+	return lambda x: ((fn(x - dx) + fn(x) + fn(x + dx)) / 3)
 
 glattad_sin = glatta(math.sin)
 
-def upprepa(func, n, val):
-	sum = 0
-	for i in range(0, n):
-		sum += func(val - dx) + func(val) + func(val + dx)
-	print(sum, n)
-	return sum / n
-
-def mangfalt_glattad(func, n):
-	return lambda x: upprepa(glatta(func), n, x)
-	
-
-
+def mangfalt_glattad(fn, n):
+	for i in range(n):
+		fn = glatta(fn)
+	return fn
 	
 mangfald = mangfalt_glattad(math.sin, 5)
 
